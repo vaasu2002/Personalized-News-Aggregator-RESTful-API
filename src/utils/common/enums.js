065363059621e18740212198b0c16c965d9d365f
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2023-08-02 17:56:33 +0000

    news category enums

---
+++ src/utils/common/enums.js
@@ -0,0 +1,13 @@
+const CATEGORY = {
+    BUSINESS:'business',
+    ENTERTAINMENT:'entertainment',
+    GENERAL:'general',
+    HEALTH:'health',
+    SCIENCE:'science',
+    SPORTS:'sports',
+    TECHNOLOGY:'technology'
+}
+
+module.exports = {
+    CATEGORY
+}
