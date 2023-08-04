e4747618114da4f27d90843154a81bfd59e6ebb6
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-08-04 18:44:15 +0530

    made routes right

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,7 @@ const { InfoController,UserController } = require('../../controllers');
 const router = express.Router();
 
 router.get('/info', InfoController.info);
-router.post('/testing/signin',UserController.signin);
-router.post('/testing/signup',UserController.signup);
+
+router.post('/register',UserController.signup);
+router.post('/login',UserController.signin);
 module.exports = router;
